369f521dc6f6c63f26b87decbc2836f09382f0b9
Ainsley Su <vhtmscyo@gmail.com>
2026-05-30 14:29:30 +0200

    Store the user's preference in a context (local state save) so it persists

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -3,23 +3,31 @@ import { useState } from "react";
 const LS_PREFER_CELSIUM = "prefer_celsium";
 const WeatherContainer = ({ data, error }) => {
   const [preferCelcium, setPreferCelcium] = useState(
-    localStorage.getItem(LS_PREFER_CELSIUM) ?? true,
+    localStorage.getItem(LS_PREFER_CELSIUM) ?? "true",
   );
+
+  function updatePreferCelcium(preferBool) {
+    const preferString = preferBool ? "true" : "false";
+    setPreferCelcium(preferString);
+    localStorage.setItem(LS_PREFER_CELSIUM, preferString);
+  }
 
   return (
     <div className="weather-container">
       <label>
         <input
           type="checkbox"
-          value={preferCelcium}
-          onChange={(e) => setPreferCelcium(e.target.value)}
+          checked={preferCelcium === "true"}
+          onChange={(e) => updatePreferCelcium(e.target.checked)}
         />
         Celcium?
       </label>
 
       {error && <div style={{ color: "red" }}>{error}</div>}
-      {data?.location && (
+      {data?.location ? (
         <WeatherDataDisplay data={data} isCelcium={preferCelcium} />
+      ) : (
+        <p> No data here...</p>
       )}
     </div>
   );
@@ -37,7 +45,7 @@ const WeatherDataDisplay = ({ data, isCelcium }) => {
         Lat: {location.lat}, Lon: {location.lon}
       </p>
       <p>
-        {isCelcium
+        {isCelcium === "true"
           ? `Temperature: ${current?.temp_c} °C`
           : `Temperature: ${current?.temp_f} °F`}
       </p>
